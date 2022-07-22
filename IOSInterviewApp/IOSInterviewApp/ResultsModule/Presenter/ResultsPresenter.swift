b1fada829ce1cff33b9109787ac30539b7e8bd43
Grigory <grishavinyar@icloud.com>
2022-07-22 23:10:18 +0300

    Created delete elements from tableView functionality
    1. Created delete method in RealmService
    2. Used delete functionality in ResultsVC()

--- IOSInterviewApp/IOSInterviewApp/ResultsModule/Presenter/ResultsPresenter.swift
+++ IOSInterviewApp/IOSInterviewApp/ResultsModule/Presenter/ResultsPresenter.swift
@@ -12,6 +12,7 @@ protocol ResultsPresenterProtocol: AnyObject {
     func settingsButtonDidTapped()
     func pairResultsTableAndRealm(tableView: UITableView)
     var realmResults: RealmSwift.Results<Results>? { get }
+    func deleteAllElements()
 }
 
 class ResultsPresenter: ResultsPresenterProtocol {
@@ -20,6 +21,10 @@ class ResultsPresenter: ResultsPresenterProtocol {
     var realmService: RealmServiceProtocol!
     var realmResults: RealmSwift.Results<Results>?
     var token: NotificationToken?
+    
+    func deleteAllElements() {
+        self.realmService.deleteAllResultsFromRealm()
+    }
     
     func pairResultsTableAndRealm(tableView: UITableView) {
         guard let realm = try? Realm() else { return }
@@ -31,7 +36,7 @@ class ResultsPresenter: ResultsPresenterProtocol {
             case .update(_, deletions: let deletions, insertions: let insertions, modifications: let modifications):
                 tableView.beginUpdates()
                 tableView.insertRows(at: insertions.map({IndexPath(row: $0, section: $0)}), with: .automatic)
-                tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: $0) }), with: .automatic)
+                tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
                 tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: $0) }), with: .automatic)
                 tableView.endUpdates()
             case .error(let error):
