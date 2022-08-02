9fdb8ac08b8befa28cec7eb5b3da40cd2a7b7223
Grigory <grishavinyar@icloud.com>
2022-08-02 12:05:47 +0300

    Created tests for MenuViewModel
    1. Deleted old MenuPresenterProtocol
    2. Created new file MenuViewModelTests and tests in it

---
+++ IOSInterviewApp/IOSInterviewAppTests/Presenter/MenuPresenterTests.swift
@@ -0,0 +1,25 @@
+//
+//  MenuPresenterTests.swift
+//  IOSInterviewAppTests
+//
+//  Created by Григорий Виняр on 02/08/2022.
+//
+
+import XCTest
+@testable import IOSInterviewApp
+
+class MenuPresenterTests: XCTestCase {
+
+    var sut: MenuViewModel!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+
+}
